e92c7c1dcbde92a777d01621371ff75ccdbeedef
Michael Gama <mrgama7@gmail.com>
2020-09-26 01:07:31 -0500

    First commit

---
+++ src/build/precache-manifest.5154193238e7178f88a0fa42626e52a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e164f810b8c8dd17a36db82b9beb059b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "912ca746ea355f64d84d",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "5ef216fa281d149ab288",
+    "url": "/static/js/2.135e794b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.135e794b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912ca746ea355f64d84d",
+    "url": "/static/js/main.fa7b8568.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
